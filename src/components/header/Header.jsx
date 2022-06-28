687563c7191d8c86713478ea1dee6477b3a3a8a5
liu-samuel <liusamuelt@gmail.com>
2022-06-28 15:43:33 -0400

    working on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Samuel Liu</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src="{ME}" alt="me" />
+        </div>
+
+        <a href="#contant" className='scroll_down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://linkedin.com" target={}></a>
+        <a href="https://github.com" target={}></a>
+        <a href="https://dribble.com" target={}></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
