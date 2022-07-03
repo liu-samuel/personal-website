973681e948b609a61a0d777d006f3e22d41645cb
liu-samuel <liusamuelt@gmail.com>
2022-07-03 00:08:50 -0400

    header and nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+/* import ME from '../../assets/me.png' */
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className='container header_container'>
+      <div className='header_container'>
         <h5>Hello I'm</h5>
         <h1>Samuel Liu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial />
 
         <div className='me'>
           <img src="{ME}" alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { SiGmail } from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target={}></a>
-        <a href="https://github.com" target={}></a>
-        <a href="https://dribble.com" target={}></a>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com" target="_blank"><BsGithub /></a>
+        <a href="https://dribble.com" target="_blank"><SiGmail /></a>
     </div>
   )
 }
