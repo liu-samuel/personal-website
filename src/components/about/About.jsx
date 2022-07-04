929c684ddc08e37a9c6eb0486b9d26441bb45c23
liu-samuel <liusamuelt@gmail.com>
2022-07-03 22:25:32 -0400

    two more components to go

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import "./about.css"
+/* import ME from '../../assets/me-about.jpg' */
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      
+      <div className='about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            {/* <img src="{ME}" alt="About Image" /> */}
+          </div>
+
+        </div>
+      
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3 Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiUsers className='about_icon'/>
+              <h5>Experience</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Experience</h5>
+              <small>80+</small>
+            </article>
+          </div>
+
+          <p>
+            Description
+          </p>
+          
+          <a href="#contact" className='btn-primary'></a>
+
+        </div>  
+      </div>
+      
+      
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,18 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='portfolio_container'>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn'>Github</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
